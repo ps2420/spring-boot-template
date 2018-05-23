8b56ab0bfaf480972be7bfb6b9649edfe9465d3a
ps2420 <pankaj.pankasin@gmail.com>
2018-05-23 12:48:44 +0800

    addign kafka client

--- web-api/src/test/java/com/memorynotfound/ITTestSetup.java
+++ web-api/src/test/java/com/memorynotfound/ITTestSetup.java
@@ -1,16 +1,11 @@
 package com.memorynotfound;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
-
-import com.memorynotfound.cloud.WebApiRunner;
 
 @SpringBootApplication
 @Configuration
-@ComponentScan(basePackages = {}, excludeFilters = {@Filter(type = FilterType.ASSIGNABLE_TYPE, value= {WebApiRunner.class})})
+//@ComponentScan(basePackages = {}, excludeFilters = {@Filter(type = FilterType.ASSIGNABLE_TYPE, value= {WebApiRunner.class})})
 public class ITTestSetup {
 
 }
